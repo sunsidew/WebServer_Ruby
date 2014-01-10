9e1dfad772040d2ed13950f6636e930ea6282286
yangjeewoong <danielay@naver.com>
2014-01-10 22:10:38 +0900

    POST 메소드로 test03(action.html) 코드 상에 form data 받아서 파싱 및 삽입 성공

--- webserv.rb
+++ webserv.rb
@@ -5,22 +5,58 @@ class HttpServer
     @session = session
     @request = request
     @pwd = pwd
+
+    #Content-Length로 가져온 값을 저장할 스트링
+    @formcontent = ""
+  end
+
+  def formparse(buffer)
+    #html 코드(예제 = action.html) 상에서 특정 문자열 위치에 가져온 값을 파싱하여 코드에 삽입
+    indexstring = "<p>an empty stub</p>"
+    
+    if buffer.gsub!(indexstring)
+      @formcontent.split('&').each do |form|
+        buffer.gsub!(indexstring,"<h3>"+form.split('=')[0]+" : "+form.split('=')[1]+"</h3>"+indexstring)
+      end
+
+      buffer.gsub!(indexstring,'')
+    end
+    
+    return buffer
   end
 
   def parse()
     fileName = nil
+    readline = nil
+    cont_len = nil
     
+    # POST메소드로 넘겨받은 값을 Content-Length를 가지고 세션에서 form data를 분리
+    if @request =~ /POST .* HTTP.*/
+      loop do
+        readline = @session.gets.strip
+        cont_len = readline.gsub(/Content-Length: /,'').to_i if readline =~ /Content-Length: .*/
+        
+        if readline.length == 0
+          cont_len.times do
+            @formcontent << @session.getc
+          end
+          
+          break
+        end
+      end
+    end
+
     # 세션에서 가져온 리퀘스트의 메서드와 http프로토콜 규격에서 웹서버가 가져올 파일 이름을 추출
     if @request =~ /GET .* HTTP.*/ or @request =~ /POST .* HTTP.*/
       fileName = @request.gsub(/GET /, '').gsub(/POST /, '').gsub(/ HTTP.*/, '')
     end
 
-    # 파일명에서 "\n" 문자 제거 및 요청하는 파일명을 현재 경로와 합쳐줌
+    # 파일명에서 \n 문자 제거 및 요청하는 파일명을 현재 경로와 합쳐줌
     # strip은 trim과 거의 같은 기능을 수행
     fileName = @pwd + fileName.strip
 
     #요청하는 경로 == 폴더일때 index.html을 파일 이름에 추가해줌
-    fileName << "/index.html" if File.directory?(fileName)
+    fileName << "index.html" if File.directory?(fileName)
 
     return fileName
   end
@@ -38,6 +74,9 @@ class HttpServer
           src = File.open(@fullPath, "rb")
           while (not src.eof?)
             buffer = src.read(256)
+
+            #가져온 form 값을 파싱해 html 코드(test03예제 action.html)로 추가
+            buffer = formparse(buffer)
             @session.write(buffer)
           end
         else
@@ -66,9 +105,9 @@ class HttpServer
     return "image/bmp"  if ext == ".bmp"
 
     #텍스트 및 기타 웹 파일 반환
-    return "text/html"  if ext == ".html" or ext == ".htm"
-    return "text/plain" if ext == ".txt"
-    return "text/css"   if ext == ".css"
+    return "text/html"       if ext == ".html" or ext == ".htm"
+    return "text/plain"      if ext == ".txt"
+    return "text/css"        if ext == ".css"
     return "text/javascript" if ext == ".js"
 
     return "text/plain" if ext == ".rb"
@@ -86,9 +125,8 @@ pwd = File.expand_path(dirname, @defaultPath) # 원격 경로에서도 웹서버
 loop do
   session = server.accept # 서버 접속 시도(성공)를 수용함
   request = session.gets # 연결로부터 요청하는 형식과 메소드, 규약 등을 받아옴
-
+  
   Thread.start(session, request) do |session, request|
     HttpServer.new(session, request, pwd).start()
   end
 end
-log.close
